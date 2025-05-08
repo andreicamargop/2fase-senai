6737c7f99fb08d07fa15919f79ce67adec883aee
ANDREI CAMARGO PINTO <andrei_c_pinto@estudante.sesisenai.org.br>
2025-05-07 21:57:59 -0300

    'atu'

---
+++ Aula0605/imesa/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [valorImput, setValorImput] = useState()
+  const [ValorTotal, setValorTotal] = useState (0)
+
+function credito(){
+  setValorTotal(ValorTotal + Number(valorImput))    
+  }
+
+function debito(){
+  setValorTotal(ValorTotal - Number(valorImput))
+}
+  
+
+  return (
+    <>
+      <div className='body'>
+
+      <div className='quadro'>
+
+        <div> 
+          <h1>Imesada</h1>
+          <h4>Controlinho financeiro</h4>
+        </div>
+        
+          <div className='saldo'>
+           <h2>💰Saldo: R${ValorTotal.toFixed(2)}</h2> 
+          </div>
+
+          <div className='botoes'>
+        
+            <button onClick={debito} className='botao-debito'>Débito</button>
+            <button onClick={credito} className='botao-credito'>Credito</button>
+            
+          </div>
+
+            <div>
+              <input type="Number"
+              value={valorImput}
+              onChange={(e) => setValorImput(e.target.value)}/>
+            </div>
+        
+        </div>
+      
+      </div>
+
+      
+
+    
+    </>
+  )
+}
+
+export default App
